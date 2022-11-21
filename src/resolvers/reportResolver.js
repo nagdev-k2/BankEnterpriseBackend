7abd160f5b0ad6b82034981a7f9ef63e10359499
Vijay Ganesh <vijay.ganehp95@gmail.com>
2022-11-20 20:23:15 -0600

    Ques 5 Fixes

--- src/resolvers/reportResolver.js
+++ src/resolvers/reportResolver.js
@@ -6,7 +6,7 @@ const reportQueries = {
     await connection.promise().query('select BRANCH_ID from BRANCH').then(async([rows, fields]) => {
       for(let i = 0; i < rows.length; i++) {
         await connection.promise().query(`SELECT
-          ACCOUNT_DETAILS.BRANCH_ID, COUNT(LOAN.LOAN_NO) AS TOTAL_LOANS,
+          ACCOUNT_DETAILS.BRANCH_ID, COUNT(DISTINCT(LOAN.LOAN_NO)) AS TOTAL_LOANS,
           SUM(LOAN.BALANCE) - COALESCE(SUM(LOAN_PAYMENTS.AMOUNT), 0) AS OUTSTANDING_BALANCE,
           ACCOUNT_DETAILS.TOTAL_ACCOUNTS, ACCOUNT_DETAILS.TOTAL_BALANCE
           FROM
@@ -26,7 +26,7 @@ const reportQueries = {
   },
   async getWeeklyReport(_, args) {
     let res = [];
-    await connection.promise().query(`select LOAN.BRANCH_ID, DATE_SUB(CURDATE(), INTERVAL WEEKDAY(CURDATE()) DAY) AS WEEK_START_DATE ,LOAN.LOAN_OFFICER_SSN,count(LOAN.LOAN_OFFICER_SSN) as NO_OF_LOANS_HANDLED,LOAN.LOAN_TYPE, SUM(LOAN_PAYMENTS.AMOUNT) AS AMOUNT_DEPOSITED from LOAN,LOAN_PAYMENTS where LOAN.LOAN_NO=LOAN_PAYMENTS.LOAN_NO and LOAN_PAYMENTS.DATE between DATE_SUB(CURDATE(), INTERVAL WEEKDAY(CURDATE()) DAY) and CURDATE() group by LOAN.LOAN_OFFICER_SSN`).then(([rows, fields]) => {
+    await connection.promise().query(`select LOAN.BRANCH_ID, DATE_SUB(CURDATE(), INTERVAL WEEKDAY(CURDATE()) DAY) AS WEEK_START_DATE ,LOAN.LOAN_OFFICER_SSN,count(LOAN.LOAN_OFFICER_SSN) as NO_OF_LOANS_HANDLED,LOAN.LOAN_TYPE, SUM(LOAN_PAYMENTS.AMOUNT) AS AMOUNT_DEPOSITED from LOAN,LOAN_PAYMENTS where LOAN.LOAN_NO=LOAN_PAYMENTS.LOAN_NO and LOAN_PAYMENTS.DATE between DATE_SUB(CURDATE(), INTERVAL WEEKDAY(CURDATE()) DAY) and CURDATE() group by LOAN.BRANCH_ID, LOAN.LOAN_TYPE, LOAN.LOAN_OFFICER_SSN`).then(([rows, fields]) => {
       console.log(rows);
       res = rows
     });
